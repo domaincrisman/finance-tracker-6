c34e167b5c8e3ef4e0af1a3cbc9525bdeabf89c7
domaincrisman <cmanzanore@gmail.com>
2020-05-07 01:38:45 +0200

    Fix Api Conexion

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,12 +9,12 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(
                                   publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   secret_token: Rails.application.credentials.iex_client[:secret_api_key],
-                                  endpoint: 'https://sandbox.iexapis.com/v1'
+                                  endpoint: 'https://sandbox.iexapis.com/stable'
                                   )
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
-        print exception.backtrace.join("\n")
+      return nil
     end
   end
 
