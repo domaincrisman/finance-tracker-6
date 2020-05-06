989561f18866d62943499e0822eb0ad48217f981
domaincrisman <cmanzanore@gmail.com>
2020-05-06 23:57:02 +0200

    Add print exception

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -14,7 +14,7 @@ class Stock < ApplicationRecord
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
-      return nil 
+        print exception.backtrace.join("\n")
     end
   end
 
